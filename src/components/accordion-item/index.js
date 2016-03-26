3b42a5ab5c5b36e186c11696c13e994ca87baf08
tyllo <gmtyllo@gmail.com>
2016-03-27 01:33:46 +1000

    [deprecated] components: accordion-item ans media-list

--- src/components/accordion-item/index.js
+++
@@ -1,8 +0,0 @@
-import template from './template.jade'
-
-export let  name = 'accordion-item'
-
-export default {
-  props: ['title'],
-  template: template(),
-}
--- src/pages/contacts/index.js
+++ src/pages/contacts/index.js
@@ -6,6 +6,20 @@ import i18n from './i18n'
 import template from './template.jade'
 
 var name = 'contacts'
+var typeList = {
+  email: {
+    icon: 'email',
+    href: 'mailto:',
+  },
+  phone: {
+    icon: 'call',
+    href: 'tel:',
+  },
+  address: {
+    icon: 'locate',
+    href: 'http://maps.google.com?q=',
+  },
+}
 
 store.actions.setLocal({name, i18n})
 
@@ -15,4 +29,30 @@ export default {
   data: () => ({
     terminals: [seaTerminal, dryTerminal]
   }),
+  filters: {
+    normilize(list) {
+      return list.map( item => {
+        var type = item.type
+        item.component = 'item-content'
+
+        if (type !== undefined) {
+          item.icon = 'empty'
+        }
+
+        if (Object.keys(typeList).indexOf(type) !== -1) {
+          item.component = 'item-content-link'
+          item.icon = typeList[type].icon
+          item.href = typeList[type].href + item.text
+          item.classLink = 'external'
+        }
+
+        if (item.content) {
+          item.component = 'item-content-accordion'
+          item.class = 'accordion-item'
+        }
+
+        return item
+      })
+    },
+  },
 }
